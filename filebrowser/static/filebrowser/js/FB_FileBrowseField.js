d9ac18e2cd4a962af5107331026afece5420bdb7
Vladius25 <vkorol2509@icloud.com>
2020-08-02 02:24:48 +0500

    Add player for audio files

--- filebrowser/static/filebrowser/js/FB_FileBrowseField.js
+++ filebrowser/static/filebrowser/js/FB_FileBrowseField.js
@@ -9,9 +9,16 @@ function FileSubmit(FilePath, FileURL, ThumbURL, FileType) {
     preview = opener.document.getElementById(preview_id);
     previewlink = opener.document.getElementById(previewlink_id);
     previewimage = opener.document.getElementById(previewimage_id);
+    previewplayer = opener.document.getElementById("player");
     // set new value for input field
     input.value = FilePath;
-    
+
+    if (previewplayer)
+        previewplayer.remove()
+    previewlink.setAttribute("href", "");
+    previewlink.setAttribute("target", "");
+    previewimage.setAttribute("src", "");
+    preview.setAttribute("style", "display:none");
     if (ThumbURL && FileType != "") {
         // selected file is an image and thumbnail is available:
         // display the preview-image (thumbnail)
@@ -20,12 +27,9 @@ function FileSubmit(FilePath, FileURL, ThumbURL, FileType) {
         previewlink.setAttribute("target", "_blank");
         previewimage.setAttribute("src", ThumbURL);
         preview.setAttribute("style", "display:block");
-    } else {
-        // hide preview elements
-        previewlink.setAttribute("href", "");
-        previewlink.setAttribute("target", "");
-        previewimage.setAttribute("src", "");
-        preview.setAttribute("style", "display:none");
+    } else if (FileType == "Audio"){
+        preview.innerHTML += "<audio id=\"player\" src="+FileURL+" controls>Your browser does not support the audio element.</audio>"
+        preview.setAttribute("style", "display:block");
     }
     this.close();
 }
